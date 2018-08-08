03c36c8b6ed554fa7fd54e992fec4043bbc4ab69
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-08-08 15:55:12 +0900

    Mondai3 piko binary not runnnig.For Ubuntu Build code.

---
+++ Mondai3/mondai3piko/cmake-build-debug/CMakeFiles/mondai3piko.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mondai3piko.dir/main.cpp.o"
+  "mondai3piko.pdb"
+  "mondai3piko"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mondai3piko.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Mondai3/mondai3piko/main.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <cstdint>
+#include <stdint.h>
+
+int auth(char *user, char *pass)
+{
+    printf("user: %s\n", user);
+    printf("pass: %s\n", pass);
+}
+
+int evil()
+{
+    printf("H@ck3d!!!\n");
+    exit(1);
+}
+
+int input()
+{
+    int a = 0x33333333;
+    int b = 0x44444444;
+    char user[8] = "";
+    char pass[8] = "";
+
+    setreuid(geteuid(), -1);
+
+    //printf("addr: 0x%08x\n", (unsigned int)pass);
+    printf("addr: 0x%08x\n", (uint64_t)pass);
+    scanf("%s", user);
+    scanf("%s", pass);
+
+    auth(user, pass);
+}
+
+int main() {
+    input();
+}
